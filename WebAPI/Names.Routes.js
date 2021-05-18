9486fd9f5306de570055af6110077f7eaaf24324
fouad-abdeen <75083392+fouad-abdeen@users.noreply.github.com>
2021-05-18 22:33:41 +0300

    Update Names.Routes.js

--- WebAPI/Names.Routes.js
+++ WebAPI/Names.Routes.js
@@ -2,8 +2,6 @@ const routes = {
   CREATE_CONTACT: "createContact",
   GET_SOCIAL_ACCOUNTS: "getSocialAccounts",
   GET_TITLES: "getTitles",
-  GET_FORM_ACTIONS: "getFormActions",
-  GET_FORM_MESSAGES: "getFormMessages",
 };
 
 module.exports = routes;
